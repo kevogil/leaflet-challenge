7aa35d6b20ff0ab8f3d251f2ad4746c7d8405f0b
Kevin Gil <kevingil23@gmail.com>
2021-10-08 21:35:16 -0700

    final edits

--- static/js/logic.js
+++ static/js/logic.js
@@ -76,11 +76,11 @@ function createMap(markers) {
       });
     
     // Dark tile layer for map background
-    var darkmap = L.tileLayer("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png", {
-    attribution: '© <a href="https://stadiamaps.com/">Stadia Maps</a>, © <a href="https://openmaptiles.org/">OpenMapTiles</a> © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-    maxZoom: 18
-    // id: "mapbox.dark",
-    // accessToken: API_KEY
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
     });
 
     // Create a baseMaps object to hold the tile layers
